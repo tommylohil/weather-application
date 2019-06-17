794db7912c7f5193021eb838e9e3a7ec10d7fcf5
Tommy Lohil <tommylohil23052000@gmail.com>
2019-06-17 15:09:07 +0700

    =Init commit

---
+++ public/js/app.js
@@ -0,0 +1,29 @@
+console.log('Client side javascript file.')
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+weatherForm.addEventListener('submit', (evt) => {
+    evt.preventDefault() // so it doesnt refresh the page and we can see it in console
+
+    const location = search.value
+
+    messageOne.textContent = 'Loading...'
+    messageOne.textContent = ''
+    
+
+    fetch('http://localhost:3000/weather?address='+(location)).then((res) => {
+        res.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error
+            }
+            else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+            }
+        })
+    })
+})
+
